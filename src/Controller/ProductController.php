b21cca9d758ba790e0d433923711e8234a45b35a
Julien.L <skiray501@gmail.com>
2020-06-22 00:59:28 +0200

    home page

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -10,12 +10,12 @@ class ProductController extends AbstractController
     /**
      * @Route("/product", name="product")
      */
-    public function index()
+    public function product()
     {
-        return $this->render('product/index.html.twig', [
+        return $this->render('product/product.html.twig', [
             'controller_name' => 'ProductController',
         ]);
     }
 
-    
+
 }
