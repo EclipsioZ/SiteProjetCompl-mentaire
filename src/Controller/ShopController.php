749c5537ee70a56e19a265b98fba6b4af7860f1e
unknown <flo47rossi@gmail.com>
2020-06-25 15:00:52 +0200

    Category/product/suppression

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -21,13 +21,18 @@ class ShopController extends GeneratorController
         ]);
     }
 
-    public function pageAddProduct()
+    public function pageAddProduct($id=null)
     {
+
+        if($id == null) {
+            return $this->redirect('/shop');
+        }
 
         $categories = API::call('GET', '/shop/getCategories');
         return $this->rendering('product/addProduct.html.twig', [
             'controller_name' => 'ShopController',
-            'categories' => $categories->categories
+            'categories' => $categories->categories,
+            'categorie_default' => $id
         ]);
     }
 
@@ -79,19 +84,90 @@ class ShopController extends GeneratorController
 
             // Get from API
             $response = API::call('POST', '/shop/delProduct', $data);
-
+            $displayCategories = API::call('GET', '/shop/getDisplayShop');
             if(!$response) {
-                return $this->rendering('shop/shop.html.twig', [ 'error' => 'Impossible de supprimer le produit', 'data' => $data ]);
+                return $this->rendering('shop/shop.html.twig', [ 'error' => 'Impossible de supprimer le produit', 'data' => $data, 'categories' => $displayCategories->displayCategories]);
             }
 
             if(isset($response->error)) {
-                return $this->rendering('shop/shop.html.twig', [ 'error' => $response->error, 'data' => $data ]);
+                return $this->rendering('shop/shop.html.twig', [ 'error' => $response->error, 'data' => $data, 'categories' => $displayCategories->displayCategories]);
             }
 
             return $this->redirect($this->generateUrl('shop_page'));
 
         }
         return $this->redirect($this->generateUrl('shop_page'));
+    }
+
+    public function addCategory(Request $request)
+    {
+        $data = API::process($request, [
+            'label' => true
+        ]);
+
+        if(!isset($data['error'])) {
+
+            // Get from API
+            $response = API::call('POST', '/shop/addCategory/', $data);
+            $displayCategories = API::call('GET', '/shop/getDisplayShop');
+            if(!$response) {
+                return $this->rendering('shop/shop.html.twig', [ 'error' => 'Impossible d\'ajouter la catégorie', 'data' => $data, 'categories' => $displayCategories->displayCategories]);
+            }
+
+            if(isset($response->error)) {
+                return $this->rendering('shop/shop.html.twig', [ 'error' => $response->error, 'data' => $data, 'categories' => $displayCategories->displayCategories]);
+            }
+
+            return $this->redirect($this->generateUrl('shop_page'));
+
+        }
+        return $this->redirect($this->generateUrl('shop_page'));
+    }
+
+    public function delCategory(Request $request, $id=null)
+    {
+        if($id == null) {
+            return $this->redirect('/shop');
+        }
+
+        $data=[];
+        $data['id']= $id;
+
+        if(!isset($data['error'])) {
+
+            // Get from API
+            $response = API::call('POST', '/shop/delCategory', $data);
+            $displayCategories = API::call('GET', '/shop/getDisplayShop');
+            if(!$response) {
+                return $this->rendering('shop/shop.html.twig', [ 'error' => 'Impossible de supprimer la categorie', 'data' => $data, 'categories' => $displayCategories->displayCategories]);
+            }
+
+            if(isset($response->error)) {
+                return $this->rendering('shop/shop.html.twig', [ 'error' => $response->error, 'data' => $data, 'categories' => $displayCategories->displayCategories]);
+            }
+
+            return $this->redirect($this->generateUrl('shop_page'));
+
+        }
+        return $this->redirect($this->generateUrl('shop_page'));
+    }
+
+    public function pageDisplayProduct($id=null)
+    {
+
+        if($id == null) {
+            return $this->redirect('/shop');
+        }
+
+        $data=[];
+        $data['id']= $id;
+
+        $product = API::call('POST', '/shop/getProduct', $data);
+
+        return $this->rendering('product/product.html.twig', [
+            'controller_name' => 'ShopController',
+            'product' => $product->product
+        ]);
     }
 
 }
