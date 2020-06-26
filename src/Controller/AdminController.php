f3c155a9860e1851127163797cc1029267c083c6
unknown <flo47rossi@gmail.com>
2020-06-26 16:15:48 +0200

    Admin Page, contact page + display admin page

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,15 +5,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Acme\CustomBundle\API;
+use App\Acme\CustomBundle\User;
+
+use Symfony\Component\HttpFoundation\Request;
+
 class AdminController extends GeneratorController
 {
-    /**
-     * @Route("/admin", name="admin")
-     */
+
     public function admin()
     {
+        $contacts = API::call('GET', '/contact/getContact');
+        $commands = API::call('GET', '/shop/getCommand');
+
         return $this->rendering('admin/admin.html.twig', [
             'controller_name' => 'AdminController',
+            'contacts' => $contacts->contacts,
+            'commands' => $commands->commands,
         ]);
     }
 }
--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,16 +4,45 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Acme\CustomBundle\API;
+use App\Acme\CustomBundle\User;
+
+use Symfony\Component\HttpFoundation\Request;
+
 
 class ContactController extends GeneratorController
 {
-    /**
-     * @Route("/contact", name="contact")
-     */
-    public function contact()
+    public function pageContact()
     {
         return $this->rendering('contact/contact.html.twig', [
             'controller_name' => 'ContactController',
         ]);
+    }
+
+
+    public function newContact(Request $request)
+    {
+
+        $data = API::process($request, [
+            'firstname' => true,
+            'lastname' => true,
+            'mail' => true,
+            'message' => true
+        ]);
+
+
+        if(!isset($data['error'])) {
+            $response = API::call('POST', '/contact/newContact', $data);
+            if(!$response) {
+                return $this->rendering('contact/contact.html.twig', [ 'error' => 'Impossible d\'ajouter le produit aux panier', 'data' => $data, 'categories' => $displayCategories->displayCategories]);
+            }
+
+            if(isset($response->error)) {
+                return $this->rendering('contact/contact.html.twig', [ 'error' => $response->error, 'data' => $data, 'categories' => $displayCategories->displayCategories]);
+            }
+            return $this->redirect($this->generateUrl('index_page'));
+        }
+
+        return $this->redirect($this->generateUrl('index_page'));
     }
 }
